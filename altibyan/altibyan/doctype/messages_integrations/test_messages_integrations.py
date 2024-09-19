3e3736c3ea8d3622635896965d55982500e3864b
fr3ooon1 <fero>
2024-09-19 18:03:54 +0300

    make a new customization to integration with messages

---
+++ altibyan/altibyan/doctype/messages_integrations/test_messages_integrations.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Hagar Mossad and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestMessagesIntegrations(FrappeTestCase):
+	pass
--- altibyan/api.py
+++ altibyan/api.py
@@ -1,5 +1,8 @@
 import frappe
+from frappe import _
 from erpnext.stock.utils import scan_barcode
+import requests
+import json
 
 
 @frappe.whitelist()
@@ -9,3 +12,48 @@ def get_active_domains():
 @frappe.whitelist()
 def scan_batch_barcode(barcode): 
 	pass
+
+
+def send_sms_message(to_number, message):
+
+	doc = frappe.get_doc("Messages Integrations")
+	
+	username = doc.username
+	password = doc.password
+	sendername = doc.sendername
+	message = message
+
+
+	url = f"""{doc.sms_url}?username={username}&password={password}&sendername={sendername}&mobiles={to_number}&message={message}"""
+
+	r = requests.get(url)
+	if r.status_code == 200:
+		print("Message sent successfully!")
+		return r
+	else:
+		print(f"Failed to send message. Status code: {r.status_code}, Response: {r.text}")
+
+def send_whatsapp_message(to_number, message):
+	doc = frappe.get_doc("Messages Integrations")
+	production_id = doc.production_id
+	phone_id = doc.phone_id
+	url = f"""{doc.whatsapp_url}/{production_id}/{phone_id}/sendMessage"""
+	
+	headers = {
+		"Content-Type": "application/json",
+		"x-maytapi-key": doc.token
+	}
+	
+	payload = {
+		"to_number": to_number,
+		"type": "text",
+		"message": message
+	}
+
+	response = requests.post(url, headers=headers, data=json.dumps(payload))
+	
+	if response.status_code == 200:
+		print("Message sent successfully!")
+		return response
+	else:
+		print(f"Failed to send message. Status code: {response.status_code}, Response: {response.text}")
