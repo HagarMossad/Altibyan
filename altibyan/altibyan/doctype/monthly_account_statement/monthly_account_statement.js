5f5e3f17a37b472adc67562c483e5cf1876bfb82
Esraa <esraaelzoghby11@gmail.com>
2024-09-26 13:45:01 +0300

    fixing

--- altibyan/altibyan/doctype/monthly_account_statement/monthly_account_statement.js
+++ altibyan/altibyan/doctype/monthly_account_statement/monthly_account_statement.js
@@ -19,8 +19,48 @@ frappe.ui.form.on("Monthly Account Statement", {
             "to_date": formatted_today,
             "from_date": formatted_from_date,
             "party_type": "Customer" ,
-
-            // "account":,
         });
+    },
+    sms_btn: function(frm) {
+        frappe.call({
+            method: "altibyan.controllers.monthly_account_statement.send_sms",
+            args:{
+                "name": frm.docname,
+                "customer": frm.doc.customer
+            }, 
+            callback: function (r) {
+                if (r.message && r.message.length) {
+                    
+                }
+            }
+        })
+        
+    },
+    whats_btn: function(frm) {
+        frappe.call({
+            method: "altibyan.controllers.monthly_account_statement.send_whattsapp",
+            args:{
+                "name": frm.docname,
+                "customer": frm.doc.customer
+            }, 
+            callback: function (r) {
+                if (r.message && r.message.length) {
+                    
+                }
+            }
+        })
+        
+    },
+    accepted: function(frm) {
+        if (frm.doc.accepted) {
+            frm.set_value('refused', 0); 
+        }
+    },
+
+    refused: function(frm) {
+        if (frm.doc.refused) {
+            frm.set_value('accepted', 0); 
+        }
     }
+
 });
